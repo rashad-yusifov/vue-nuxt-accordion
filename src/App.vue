3e13096558451ff40b86d6be4e4b49a571546d9a
User <planerhobbi551@gmail.com>
2021-08-27 17:10:45 +0400

    First push By Rashad-Yusifov

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <V-accordion>
+          <template v-slot:title>
+            <span
+             style="color: white"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis
+              feugiat velit feugiat quis. In.</span
+            >
+          </template>
+          <template v-slot:content>
+            <span style="color: white">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+              consequuntur quasi sapiente vero? Possimus similique nesciunt nostrum
+              temporibus quia vitae corrupti recusandae magni corporis hic modi rem
+              doloribus ea id, ad distinctio qui quis accusamus accusantium animi
+              veniam.
+            </span>
+          </template>
+    </V-accordion>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Vaccordion from '@/components/Accordion.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'V-accordion': Vaccordion
   }
 }
 </script>
